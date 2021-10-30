98fa7a572f076b2a9c23a0f5444a34423e0c1330
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2021-10-31 00:23:52 +0300

    Lesson2

---
+++ Lesson2/IThreadPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public interface IThreadPool : IDisposable
+    {
+        int Count { get; }
+
+        void QueueTask(Action task);
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+       
+        private static void Main()
+        {
+            int maxPull = 8;
+            using (var threadPool = new MyThreadPool(maxPull))
+            {
+                Console.WriteLine($"Количество Thread Pool: {threadPool.Count}");
+                Random rnd = new Random();
+                for (var i = 0; i < maxPull; i++)
+                {
+                    threadPool.QueueTask(() =>
+                    {
+                        var sleep = rnd.Next(1, 40) * 100;
+                        Thread.Sleep(sleep);
+                        Console.WriteLine($"Работа Thread {Thread.CurrentThread.ManagedThreadId} с названием {Thread.CurrentThread.Name} закончина {sleep} ms");
+                    });
+                }                
+            }
+        }
+        
+    }
+
+
+
+
+    
+}
