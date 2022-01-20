08255aa7db90f757f11dac705e4b256b864fe70a
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-01-20 16:53:27 +0300

    Lesson7

---
+++ Lesson7/Lesson7/DiskInfo.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Lesson7
+{
+    public class DiskInfo
+    {
+            public byte DiskIndex { get; set; }
+            public string DiskName { get; set; }
+            public string DriveFormat { get; set; }
+            public string DriveType { get; set; }
+            public bool IsReady { get; set; }
+            public string RootDirectory { get; set; }
+            public string VolumeLabel { get; set; }
+            public long AvailableFreeSpace { get; set; }
+            public string FreeSpace { get; set; }
+            public long CurrentDiskSize { get; set; }
+    }
+}
---
+++ Lesson7/Lesson7/ReportService.cs
@@ -0,0 +1,42 @@
+﻿using TemplateEngine.Docx;
+
+namespace Lesson7
+{
+    public class ReportService
+    {
+        public void GenerateReport(DiskInfo fileInfo, string nameFile = null)
+        {
+            if (string.IsNullOrWhiteSpace(nameFile))
+            {
+            nameFile = Path.Combine(Directory.GetCurrentDirectory(), $"Disk{fileInfo.DiskIndex.ToString()}_FileSystemReport.docx");
+            }
+            if (File.Exists(nameFile))
+            {
+                File.Delete(nameFile);
+            }
+            File.Copy("..\\..\\..\\Template.docx", nameFile);
+            var fillContetn = new Content(
+               new FieldContent("Disk Index", fileInfo.DiskIndex.ToString()),
+               new FieldContent("Disk Name", fileInfo.DiskName),
+               new FieldContent("Drive Format", fileInfo.DriveFormat),
+               new FieldContent("Drive Type", fileInfo.DriveType),
+               new FieldContent("IsReady", fileInfo.IsReady.ToString()),
+               new FieldContent("Root Directory", fileInfo.RootDirectory),
+               new FieldContent("Volume Label", fileInfo.VolumeLabel),
+               new FieldContent("Disk Size", fileInfo.CurrentDiskSize.ToString()),
+               new FieldContent("Free bytes", fileInfo.AvailableFreeSpace.ToString())
+               );
+
+            using (var outputDocument =
+                new TemplateProcessor(nameFile)
+                .SetRemoveContentControls(true))
+
+            {
+                outputDocument.FillContent(fillContetn);
+                outputDocument.SaveChanges();
+            }
+
+        }
+
+    }
+}
