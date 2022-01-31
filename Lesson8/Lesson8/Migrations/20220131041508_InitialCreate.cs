cb922c0b51824ffab599b92adce71e379b7fa21c
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-01-31 08:11:25 +0300

    Lesson8

---
+++ Lesson8/Lesson8/Migrations/20220131041508_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson8.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Office",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseWork = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Office", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Office");
+        }
+    }
+}
---
+++ Lesson8/Lesson8/Models/Office.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson8.Models
+{
+    public class Office
+    {
+            public int Id { get; set; }
+            public string? Title { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime ReleaseWork { get; set; }
+            public string? location { get; set; }
+            public string? Type { get; set; }
+            public string? Time { get; set; }
+
+    }
+}
